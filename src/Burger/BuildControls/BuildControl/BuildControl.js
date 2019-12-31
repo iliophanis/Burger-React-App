b8fc1ecc01a17fa9020986a628609b2088be7bb9
iliophanis <iliophanis@gmail.com>
2020-01-01 01:05:51 +0200

    new eraaa

---
+++ src/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BuildControl.css';
+const buildControl =(props)=>
+(
+    <div className="BuildControl">
+        <div className="Label">{props.label}</div>
+        <button onClick={props.removed} className="Less" disabled={props.disabled}>Less</button>
+        <button onClick={props.added} className="More">More</button>
+    </div>
+);
+
+export default buildControl;
